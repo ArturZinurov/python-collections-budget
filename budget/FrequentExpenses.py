defdce5fe094b2046637a51658af99f89e7cb869
Artur Zinurov <38508285+ArturZinurov@users.noreply.github.com>
2021-03-25 17:09:34 -0500

    Module 2

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,9 @@
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.cvs')
+
+spending_categories = []
+
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+
