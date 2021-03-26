b2d82ae3e19b4bfbf52676ed8dff24c5ba6cbce5
Artur Zinurov <38508285+ArturZinurov@users.noreply.github.com>
2021-03-26 11:14:47 -0500

    checking excersise 3-6

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,8 +1,15 @@
 from . import Expense
+import collections
+import matplotlib.pyplot as plt
 
 expenses = Expense.Expenses()
-expenses.read_expenses('data/spending_data.cvs')
+expenses.read_expenses('data\spending_data.csv')
 spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
+spending_conuter = collections.Counter(spending_categories)
+top5 = spending_conuter.most_common(5)
 
+categories, count = zip(*top5)
+
+fig , ax = plt.subplots()
