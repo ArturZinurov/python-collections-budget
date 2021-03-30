745c96562565eb05dca6e024d0ec31c1a62a4a11
Artur Zinurov <38508285+ArturZinurov@users.noreply.github.com>
2021-03-30 11:52:12 -0500

    fixes for module 3

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -46,11 +46,11 @@ def main():
     for entry in myBudgetList:
         print(entry)
 
-    fig , ax = plt.subplots()
-    labels = 'Expenses', 'Overages', 'Budget'
-    values = myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget
+    fig, ax = plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
     ax.bar(labels, values, color=['green', 'red', 'blue'])
-    ax.set_title('You total expenses vs. total budget')
+    ax.set_title('Your total expenses vs. total budget')
     plt.show()
 
 
